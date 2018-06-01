637e21d45d32d0a255e1277b82190933e394d9f0
Luke Horgan <horgan.l@husky.neu.edu>
2018-06-01 12:15:47 -0400

    fix merkle trees

--- models.py
+++ models.py
@@ -12,29 +12,32 @@ class Transaction:
         self.coin = coin
 
 class MerkleTree:
-    def __init__(self, val, left, right):
+    def __init__(self, val=None, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-    def build_merkle_tree(leaves):
+    def build_merkle_tree(self, leaves):
         if len(leaves) % 2 != 0:
             leaves.append(None)
 
         if len(leaves) == 2:
             self.left = leaves[0]
             self.right = leaves[1]
-            self.val = hash(leaves)
+            self.val = hash([self.get_val(leaves[0]), self.get_val(leaves[1])])
         else:
             new_leaves = []
             for i in range(0, len(leaves), 2):
-                try:
-                    sub_tree = MerkleTree(hash([leaves[i].val, leaves[i + 1].val]), leaves[i], leaves[i + 1])
-                except AttributeError:
-                    sub_tree = MerkleTree(hash([leaves[i], leaves[i + 1]), leaves[i], leaves[i + 1])
+                sub_tree = MerkleTree(hash([self.get_val(leaves[i]), self.get_val(leaves[i + 1])]), leaves[i], leaves[i + 1])
                 new_leaves.append(sub_tree)
 
-            build_merkle_tree(new_leaves)
+            self.build_merkle_tree(new_leaves)
+
+    def get_val(self, item):
+        try:
+            return item.val
+        except AttributeError:
+            return item
 
 def hash(items):
     concat = ""
